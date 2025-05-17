ebadfcd8cf6f268b36c708f9e218c0c13705b0c4
0xAA55 <weixiaobao213@qq.com>
2025-05-18 00:59:54 +0800

    修改一下公共模块。

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
 pub mod vorbis;
-
-#[macro_use]
 pub mod bitwise;
 
-pub mod mdct;
+mod utils;
+
 mod codebook;
 mod floor;
+mod mapping;
+mod residue;
 mod psy;
+
+pub use utils::*;
+pub use bitwise::*;
 
 pub const SHOW_DEBUG: bool = false;
 pub const DEBUG_ON_READ_BITS: bool = false;
 pub const DEBUG_ON_WRITE_BITS: bool = false;
 pub const PANIC_ON_ERROR: bool = false;
-pub mod envelope;
 

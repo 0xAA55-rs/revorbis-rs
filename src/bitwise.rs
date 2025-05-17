a22b96757b0d7d0bc3898a42ba0d8880fd265871
0xAA55 <weixiaobao213@qq.com>
2025-05-18 00:49:32 +0800

    `vorbis` 和 `bitwise` 里面所有具有辅助性质的函数和宏移动到 `utils`，`vorbis` 里面用到的那三个不再使用的函数也移动过去。

--- src/bitwise.rs
+++ src/bitwise.rs
@@ -1,17 +1,6 @@
 #![allow(dead_code)]
 use std::fmt::{self, Debug, Formatter};
 
-/// * Format array in a specific patterns
-#[macro_export]
-macro_rules! format_array {
-    () => {
-        "".to_string()
-    };
-    ($data:expr, $delims:expr, $($arg:tt)*) => {
-        $data.iter().map(|&v|format!($($arg)*, v)).collect::<Vec<_>>().join($delims)
-    }
-}
-pub use format_array;
 
 const MASK8: [u8; 9] = [0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF];
 

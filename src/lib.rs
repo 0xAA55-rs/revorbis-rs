64569a700b7b8dcbd53156b8b2912b2a203dc92e
0xAA55 <weixiaobao213@qq.com>
2025-05-18 00:55:33 +0800

    调试开关相关的宏移动到 `lib`

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,9 @@ pub mod bitwise;
 pub mod mdct;
 mod codebook;
 
+pub const SHOW_DEBUG: bool = false;
+pub const DEBUG_ON_READ_BITS: bool = false;
+pub const DEBUG_ON_WRITE_BITS: bool = false;
+pub const PANIC_ON_ERROR: bool = false;
 pub mod envelope;
 

0783eab47d96bc66830e0a37ad53c2e70c469442
0xAA55 <weixiaobao213@qq.com>
2025-05-20 03:52:19 +0800

    去除繁琐的手动构造。

--- src/blocks.rs
+++ src/blocks.rs
@@ -53,21 +53,8 @@ where
 {
     pub fn new(vorbis_dsp_state: &'a VorbisDspState<'a, 'b, 'c, W>, writer: W, ogg_stream_id: u32) -> Self {
         Self {
-            pcm: Vec::new(),
             ogg_stream_writer: OggStreamWriter::new(writer, ogg_stream_id),
-            lw: 0,
-            w: 0,
-            nw: 0,
-            pcmend: 0,
-            mode: 0,
-            eofflag: false,
-            granulepos: 0,
-            sequence: 0,
             vorbis_dsp_state,
-            glue_bits: 0,
-            time_bits: 0,
-            floor_bits: 0,
-            res_bits: 0,
             internal: if vorbis_dsp_state.for_encode {
                 Some(VorbisBlockInternal {
                     pcmdelay: Vec::new(),
@@ -76,7 +63,8 @@ where
                 })
             } else {
                 None
-            }
+            },
+            ..Default::default()
         }
     }
 }

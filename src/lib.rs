3a6a8cd8b75d5b9bb5b7ce8925bbfda79f070fef
0xAA55 <weixiaobao213@qq.com>
2025-05-21 02:41:05 +0800

    添加单元测试代码，并且设置 `PANIC_ON_ERROR: bool = true;`，所有的 `return_Err!()` 会触发 panic 以便于调试。

--- src/lib.rs
+++ src/lib.rs
@@ -27,8 +27,29 @@ pub const PACKETBLOBS: usize = 15;
 pub const SHOW_DEBUG: bool = false;
 pub const DEBUG_ON_READ_BITS: bool = false;
 pub const DEBUG_ON_WRITE_BITS: bool = false;
-pub const PANIC_ON_ERROR: bool = false;
+pub const PANIC_ON_ERROR: bool = true;
 
 mod no_usage;
 
 pub use headers::get_vorbis_headers_from_ogg_packet_bytes;
+
+pub use codec::{VorbisInfo, VorbisDspState};
+
+#[test]
+fn test_ogg_vorbis() {
+	use std::{
+		fs::File,
+		io::BufReader,
+	};
+	use ogg::OggStreamReader;
+	use savagestr::prelude::*;
+	let text_codecs = StringCodecMaps::new();
+	let mut oggreader = OggStreamReader::new(BufReader::new(File::open("test.ogg").unwrap()));
+	let (identification_header, comment_header, setup_header) = headers::read_vorbis_headers(&mut oggreader, &text_codecs).unwrap();
+	dbg!(&comment_header);
+	let vi = VorbisInfo::new(&identification_header, &setup_header).unwrap();
+	let vd = VorbisDspState::new(vi, false).unwrap();
+	dbg!(&vd);
+}
+
+
